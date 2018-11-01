e648eb5197bef176535a51808b3ccc7030cb9081
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-11-01 11:01:36 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -371,7 +371,22 @@ client.on('message', msg => {
         })
 
 
-		
+client.on('message', ra3d => {
+var prefix = "B!";
+                        let args = ra3d.content.split(" ").slice(1).join(" ")
+if(ra3d.content.startsWith(prefix + 'colors')) {
+    if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
+             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
+              ra3d.channel.send(`**✅ |Created __${args}__ Colors**`);
+                  setInterval(function(){})
+                    let count = 0;
+                    let ecount = 0;
+          for(let x = 1; x < `${parseInt(args)+1}`; x++){
+            ra3d.guild.createRole({name:x,
+              color: 'RANDOM'})
+              }
+            }
+       });		
 		
 		
 		
