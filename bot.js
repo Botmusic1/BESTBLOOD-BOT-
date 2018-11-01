132794baf603f757247d12a3810fa4a40e3a80a7
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-11-01 07:23:41 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,29 +83,6 @@ client.on("message", message => {
 /////////////////////////////////////////////////Avatar
 
 
-client.on('message', message => {
-    if (message.content.startsWith("B!avatar")) {
-if(!message.channel.guild) return;
-
-        var mentionned = message.mentions.users.first();
-
-    var client;
-      if(mentionned){
-          var client = mentionned;
-      } else {
-          var client = message.author;
-
-      }
-
-        const embed = new Discord.RichEmbed()
-                           .addField('Requested by:', "<@" + message.author.id + ">")
-
-        .setColor(000000)
-        .setFooter(اسم بوتك, 'ur bot image link')
-        .setImage(${client.avatarURL})
-      message.channel.sendEmbed(embed);
-    }
-});
 
 //////////////////////////////////////////////////////////////iD
 
