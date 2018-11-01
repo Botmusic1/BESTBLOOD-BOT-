c22accb24370f446d1c37239fde04861015d49bd
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-11-01 07:48:04 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -528,7 +528,24 @@ client.on('message', message => {
   
   
   
-  
+  client.on('ready', function(){
+    client.user.setStatus("dnd");
+    var ms = 100000 ;
+    var setGame = [`Best Gamer x`,``];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);100000
+
+});
   
   
    
