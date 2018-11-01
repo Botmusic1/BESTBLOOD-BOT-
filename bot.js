7ef8e3eb85c477a5db6c006c11217a9b4cd963e4
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-11-01 07:38:52 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setActivity("Best Gamer x",{type: '`http://www.twitch.tv/KiNg66S')
+ client.user.setActivity("Best Gamer x",{type: '`http://www.twitch.tv/KiNg66S'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
