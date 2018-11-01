4c692fc8820cf4264acea91917ef8d890c1bfd4d
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-11-01 12:12:33 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -534,7 +534,34 @@ Discord API: ${client.ping.toFixed(0)} ms\`\`\``);
     });
     }
 });
+client.on('message', message => {
+var prefix = "B!";
+       if(message.content === prefix + "mutechannel") {
+                           if(!message.channel.guild) return message.reply('** This command only for servers**');
 
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
+              message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: false
+
+              }).then(() => {
+                  message.reply("**__تم تقفيل الشات__ :white_check_mark: **")
+              });
+                }
+//FIRE BOT
+    if(message.content === prefix + "unmutechannel") {
+                        if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
+              message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: true
+
+              }).then(() => {
+                  message.reply("**__تم فتح الشات__:white_check_mark:**")
+              });
+    }
+       
+});
+  
 
   
   
